fbeec1ad10e0bbaa8b510bda339f368d3882f9ed
Alaattin Bedir <alaattin.bedir@etiya.com>
2017-11-19 12:48:52 +0300

    amed

---
+++ Turkcell/Views/ProductCell.m
@@ -0,0 +1,13 @@
+//
+//  ProductCell.m
+//  Turkcell
+//
+//  Created by Alaattin Bedir on 19.11.2017.
+//  Copyright © 2017 Turkcell. All rights reserved.
+//
+
+#import "ProductCell.h"
+
+@implementation ProductCell
+
+@end
